2cfa3fe95bd235f6d3856d75ace54f1e9edc9087
MechanicianW <wangyilinchn@foxmail.com>
2017-01-05 17:16:05 +0800

    init item style

--- fullPageSlider.js
+++ fullPageSlider.js
@@ -28,29 +28,57 @@
     }
     this.currentIndex = 0
     this.wrap = document.querySelector(this.option.wrap)
-    this.wrap.style.cssText+="display:block;position:relative;width:100%;height:100%"
-    this.scrollDist = window.innerHeight
     this.pages = this.wrap.querySelectorAll(this.option.page)
     this.length = this.pages.length
+    for (var i = 0, len = this.length; i < len; i++) {
+      this.pages[i].style.cssText+="display:block;position:absolute;left:0;top:0;width:100%;height:100%"
+    }
+    var s = document.createElement('style')
+    s.innerHTML = 'html,body{width:100%;height:100%;overflow:hidden}'
+    document.head.appendChild(s);
+    s = null
+    this.wrap.style.cssText+='display:block;position:relative;width:100%;height:100%'
+    this.scrollDist = this.wrap.clientHeight
     this._init()
   }
   fullPageSlider.prototype = {
     _init: function () {
       for (var i = 1; i < this.length; i++) {
         this._outPage(i, 0)
+        this.pages[i].style.height = this.scrollDist + 'px'
       }
       this._toPage(0)
+      this.pages[0].style.height = this.scrollDist + 'px'
     },
     _toPage: function (index) {
       this.pages[index].style.cssText += '-webkit-transition-duration:' + this.option.speed + 'ms;-webkit-transform:translate3d(0, 0, 0)'
     },
     _outPage: function (index, current) {
       var offset = index - current
-      this.pages[index].style.cssText += '-webkit-transition-duration:0ms;;-webkit-transform:translate3d(0,'+ this.scrollDist*offset + 'px,0)'
+      this.pages[index].style.cssText += '-webkit-transition-duration:' + this.option.speed +'ms;-webkit-transform:translate3d(0,'+ this.scrollDist*offset + 'px,0)'
     },
-    next: function (index) {},
-    prev: function () {},
-    to: function () {}
+    _transition: function (leaveIndex, toIndex) {
+      this.currentIndex = toIndex
+      this._outPage(leaveIndex, this.currentIndex)
+      this._toPage(toIndex)
+    },
+    next: function () {
+      var index = this.currentIndex + 1
+      if (index > this.length - 1) {
+        index = 0
+      }
+      this.to(index)
+    },
+    prev: function (index) {
+      var index = this.currentIndex - 1
+      if (index < 0) {
+        index = 0
+      }
+      this.to(index)
+    },
+    to: function (index) {
+      this._transition(this.currentIndex, index)
+    }
   }
   if (typeof module === 'object') {
     module.exports = fullPageSlider
